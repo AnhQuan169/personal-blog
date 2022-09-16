b51e1a737c328d95dcabc1ca3718b55416f802bf
AnhQuan169 <anhquannguyen169@gmail.com>
2022-09-16 09:16:52 +0700

    create faker data for table users with seeding(database)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -15,11 +18,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => 123456,
+            'role' => UserRole::USER(),
+            'status' => random_int(0, 2),
+            'created_at' => Carbon::yesterday(),
+            'updated_at' => Carbon::today()
         ];
     }
 
