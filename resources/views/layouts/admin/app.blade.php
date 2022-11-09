b21b9a577dd609ac2911eb9a8ea575bd67e77cb7
AnhQuan169 <anhquannguyen169@gmail.com>
2022-11-09 23:39:19 +0700

    set up the basic layout for the dashboard

---
+++ resources/views/layouts/admin/app.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="Frest admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Frest admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>Dashboard</title>
+    <link rel="apple-touch-icon" href="{{ asset('admin/app-assets/images/ico/apple-icon-120.png.html') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('admin/app-assets/images/ico/favicon.ico') }}">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,600%7CIBM+Plex+Sans:300,400,500,600,700" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/app-assets/vendors/css/vendors.min.css') }}">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/app-assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/app-assets/css/bootstrap-extended.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/app-assets/css/colors.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/app-assets/css/components.min.css') }}">
+    <!-- END: Theme CSS-->
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/app-assets/css/core/menu/menu-types/vertical-menu.min.css') }}">
+    <!-- END: Page CSS-->
+    @stack('styles')
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+<body class="vertical-layout vertical-menu-modern 2-columns  navbar-sticky footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
+
+    <!-- BEGIN: Header-->
+    @include('layouts.admin.partials.header')
+    <!-- END: Header-->
+
+    <!-- BEGIN: Main Menu-->
+    @include('layouts.admin.partials.sidebar')
+    <!-- END: Main Menu-->
+
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+            <div class="content-header row"></div>
+            <div class="content-body">
+                <!-- Dashboard Ecommerce Starts -->
+                <section id="dashboard-ecommerce">
+                    <div class="row">
+                        <!-- Greetings Content Starts -->
+                        <div class="col-xl-12 col-md-12 col-12 dashboard-greetings">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="greeting-text">Welcome to my Blog</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- Dashboard Ecommerce ends -->
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+    <!-- BEGIN: Footer-->
+    @include('layouts.admin.partials.footer')
+    <!-- END: Footer-->
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{ asset('admin/app-assets/vendors/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js') }}"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="{{ asset('admin/app-assets/js/scripts/configs/vertical-menu-light.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/js/core/app-menu.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/js/core/app.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/js/scripts/components.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/js/scripts/footer.min.js') }}"></script>
+    <script src="{{ asset('admin/app-assets/js/scripts/customizer.min.js') }}"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+
+    <!-- END: Page JS-->
+    @stack('scripts')
+</body>
+<!-- END: Body-->
+
+</html>
---
+++ resources/views/layouts/admin/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer footer-static footer-light">
+  <p class="clearfix mb-0">
+    <span class="float-left d-inline-block">2022 &copy; QUAN</span>
+    <span class="float-right d-sm-inline-block d-none">Created by
+      <a class="text-uppercase" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Nguyen Anh Quan</a>
+    </span>
+    <button class="btn btn-primary btn-icon scroll-top" type="button">
+      <i class="bx bx-up-arrow-alt"></i>
+    </button>
+  </p>
+</footer>
